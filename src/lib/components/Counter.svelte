d0d8eaa99fc80757d683271c6875cfdde9fd8c29
PhilippeDMG <philippedamau@gmail.com>
2025-06-12 15:09:42 -0300

    add counter component

---
+++ src/lib/components/Counter.svelte
@@ -0,0 +1,9 @@
+<script>
+	let count = 0;
+	function increment() {
+		count += 1;
+	}
+</script>
+
+<h1 style="font-size:20px;margin-top:20px;">Count: {count}</h1>
+<button on:click={increment} style="background-color:red;padding:8px;">Increment</button>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
  // import { goto } from "$app/navigation";
  //  let unused = 42
  // console.log(unused)
+ import Counter from "$lib/components/Counter.svelte";
 </script>
 <svelte:head>
   <title>Welcome to SvelteKit</title>
@@ -10,3 +11,4 @@
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
 <div>hola este texto se ve?</div>
 <p style="color:green;">testing automated deploy</p>
+<Counter />
